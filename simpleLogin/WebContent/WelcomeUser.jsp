f6d6cfafe18bbe20adadf5f2f2c260b870ffb388
Daniel <d.stouthamer@live.nl>
2019-09-02 18:17:24 +0200

    Update maandag
    Login

---
+++ simpleLogin/WebContent/WelcomeUser.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		request.getSession(false);
+		if (session == null) {
+	%>
+	<a href="http://localhost:8080/simpleLogin/home.jsp"> Home</a>
+	<a href="http://localhost:8080/simpleLogin/login1.jsp"> Login</a>
+	<%
+		} else {
+			// Already created.
+	%>
+	<a href="http://localhost:8080/simpleLogin/logout.jsp"> Logout</a>
+	<%
+		}
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		
+	%>
+	<br>
+	<br>
+	<%
+		out.println("Welcome: <br> " + name + "<br>" + password);
+	%>
+</body>
+</html>
---
+++ simpleLogin/src/UserRegist.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class UserProfile
+ */
+@WebServlet("/UserRegist")
+public class UserRegist extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserRegist() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    // TODO Auto-generated method stub
+   
+  }
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    try {
+    	String uname = request.getParameter("uname");
+    	String upass = request.getParameter("upass");
+    	String sql = "insert into mydb(uname, upass) values(?, ?)";
+    	Class.forName("com.mysql.jdbc.Driver");
+    	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3308/mydb","root","");
+    	PreparedStatement ps = conn.prepareStatement(sql);
+    	ps.setString(1, uname);
+    	ps.setString(2, upass);
+    	ps.executeUpdate();
+    	PrintWriter out = response.getWriter();
+    	out.println("Succes");
+		
+    } 
+    
+    catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+    
+    }  
+	
+  }
+      
+      
+    
+    
+    
+    
+}
